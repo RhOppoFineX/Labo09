332d7b74c758f9f1acfb2b1d673b259261f56d23
RhOppoFineX <d.arias2000.eh@gmail.com>
2023-05-19 10:21:28 -0600

    first commit

--- app/src/main/java/com/example/laboratorio09/HomeFragment.kt
+++ app/src/main/java/com/example/laboratorio09/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.corutinesdemo.R
 import com.example.corutinesdemo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,6 +24,12 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO: Use binding to navigate to the other fragments
+        binding.coroutinesButton.setOnClickListener{
+            it.findNavController().navigate(R.id.action_homeFragment_to_coroutinesFragment)
+        }
+
+        binding.mainThreadButton.setOnClickListener{
+            it.findNavController().navigate(R.id.action_homeFragment_to_mainThreadFragment2)
+        }
     }
 }
